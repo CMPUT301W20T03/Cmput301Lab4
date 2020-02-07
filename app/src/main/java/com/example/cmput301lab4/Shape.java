f7df8703a795de68b1d0b48a594d3f0065219945
shuangchaowang <46541376+shuangchaowang@users.noreply.github.com>
2020-02-07 13:57:35 -0700

    add new color string by shuangchaowang

--- app/src/main/java/com/example/cmput301lab4/Shape.java
+++ app/src/main/java/com/example/cmput301lab4/Shape.java
@@ -3,5 +3,5 @@ package com.example.cmput301lab4;
 public class Shape {//edit the Shape Class for add the color
   public int x;//add x and y
   public int y;
-  String color = "red";
+  String color = "gray";
 }
